208f9b84503c6f5a99323eed88316aaf129c1f50
Malachi <ukomalachi@gmail.com>
2022-10-29 04:34:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+
+async function getData(dataToAnalyse){
+    let x_axis = [];
+    let y_axis = [];
+    //parsing the csv file to extract the year and temperature for data analysis
+    
+  const response = await fetch(dataToAnalyse);
+  const data = await response.text();  
+  const dataRow = data.split("\n").slice(1);
+    for(let i =0; i < dataRow.length; i++){
+        const row = dataRow[i]
+        const column = row.split(",");
+        const year = column[0];
+        const temp = column[1];
+        x_axis.push(year);
+        y_axis.push(temp);
+        
+    }
+    
+    console.log(`year : ${x_axis}`);
+    console.log(`temperature : ${y_axis}`);
+  return {x_axis, y_axis}
+  
+}
+
+   
+   
+    async function chartData(){
+        const data = await getData("ZonAnn.Ts+dSST (1).csv");
+        const {x_axis, y_axis} = data;
+        const ctx = document.getElementById('myChart').getContext('2d');
+        const myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: x_axis,
+                datasets: [{
+                    label: 'Combined Land-Surface Air and Sea-Surface Water Temperature Anomalies',
+                    data: y_axis,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                        
+                    borderColor: 'rgba(255, 159, 64, 1)',
+                        
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                fill : true,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+    
+chartData();
