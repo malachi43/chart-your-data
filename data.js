8e4d6bd4f7eefc24f8b08d8d0d111cde13d3ad8b
Malachi <ukomalachi@gmail.com>
2022-10-30 08:53:36 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,44 @@
+const products = [
+    {
+        id : 1,
+        product : "shoes and sneakers",
+        desc: "want to look comfy in your shoes"
+    },
+    {
+        id : 2,
+        product : "cellphones",
+        desc: "want to look comfy in your shoes"
+    },
+    {
+        id : 3,
+        product : "library",
+        desc: "want to look comfy in your shoes"
+    },
+    {
+        id : 4,
+        product : "cars and land",
+        desc: "want to look comfy in your shoes"
+    },
+    {
+        id : 5,
+        product : "shoes and sneakers",
+        desc: "want to look comfy in your shoes"
+    },
+    {
+        id : 6,
+        product : "shoes and sneakers",
+        desc: "want to look comfy in your shoes"
+    },
+    {
+        id : 7,
+        product : "shoes and sneakers",
+        desc: "want to look comfy in your shoes"
+    },
+    {
+        id : 8,
+        product : "shoes and sneakers",
+        desc: "want to look comfy in your shoes"
+    },
+]
+
+module.exports = {products}
