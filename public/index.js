c9f104d499db28360091f54e1f8e6c3f93971560
Malachi <ukomalachi@gmail.com>
2022-11-09 23:39:01 +0100

    created a csv-parser function.

--- public/index.js
+++ public/index.js
@@ -1,40 +1,19 @@
-
-async function getData(dataToAnalyse){
-    let x_axis = [];
-    let y_axis = [];
-
-    //parsing the csv file to extract the year and temperature for data analysis
-     const response = await fetch(dataToAnalyse);
-     const data = await response.text();  
-    const dataRow = data.split("\n").slice(1);
-    
-  dataRow.forEach((row)=>{
-        const column = row.split(",");
-        const year = column[0];
-        const temp = column[1];
-        x_axis.push(year);
-        y_axis.push(temp);
-    });
-
-    
-  return {x_axis, y_axis}
-   
-  
+const csvOptions = { 
+    valueForX: 1,
+    valueForY: 15
 }
-
-   
    
     async function chartData(){
-        const data = await getData("ZonAnn.Ts+dSST (1).csv");
-        const {x_axis, y_axis} = data;
+        const data = await parseCsv(csvOptions, "ZonAnn.Ts+dSST (1).csv");
+        const {graphX, graphY} = data;
         const ctx = document.getElementById('myChart').getContext('2d');
         const myChart = new Chart(ctx, {
             type: 'line',
             data: {
-                labels: x_axis,
+                labels: graphX,
                 datasets: [{
                     label: 'Combined Land-Surface Air and Sea-Surface Water Temperature Anomalies',
-                    data: y_axis,
+                    data: graphY,
                     backgroundColor: 'rgba(75, 192, 192, 0.2)',
                         
                     borderColor: 'rgba(255, 159, 64, 1)',
@@ -54,3 +33,40 @@ async function getData(dataToAnalyse){
     }
     
 chartData();
+
+
+
+
+async function parseCsv(csvParams, csvFileToParse){
+
+const graphX = [];
+const graphY = [];
+
+//we are subtracting one from the values because an array is zero indexed, so that any number chosen by the user will correspond to the given index.
+const xPlane = (csvParams.valueForX)-1;
+const yPlane = (csvParams.valueForY)-1;
+
+if(xPlane === -1 || yPlane === -1){
+    return;
+}
+
+const response = await fetch(csvFileToParse);
+const data = await response.text(); 
+const dataRow = data.split("\n");
+const header = dataRow[0].split(",")
+const csvArray = dataRow.slice(1);
+
+const newArray = csvArray.map((row)=>{
+const eachRow = row.split(",")
+const newx = eachRow[xPlane];
+const newy = eachRow[yPlane];
+    
+    graphX.push(newx);
+    graphY.push(newy)
+   
+     
+  })
+  
+
+  return {graphX, graphY}
+}
